561c3a6d59132b24fa5fcfed80e7f889adc508ee
Spencer Brazell <sa.brazell11@gmail.com>
2022-05-03 17:54:06 -0600

    saved

--- movieList/__test__/movie-list.test.js
+++ movieList/__test__/movie-list.test.js
@@ -3,7 +3,9 @@ require('chromedriver');
 
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build();
 
-const {crossOffMovie} = require('../functions/crossOffMovie')
+const {crossOffMovie} = require('../functions/crossOffMovie');
+const {deleteMovie} = require('../functions/deleteMovie');
+const {addMovie} = require('../functions/addMovie')
 
 beforeAll(async () => {
     await driver.get('http://127.0.0.1:5501/movieList/index.html');
@@ -14,6 +16,19 @@ afterAll(async () => {
     await driver.quit();
 });
 
+
+
+
 it('should cross off any movie from the list', async () => {
     await crossOffMovie(driver);
+  });
+
+
+it('should delete any movie from the list', async () => {
+    await deleteMovie(driver);
+  });
+
+
+it('should add any movie to the list', async () => {
+    await addMovie(driver);
   });
--- movieList/functions/crossOffMovie.js
+++ movieList/functions/crossOffMovie.js
@@ -20,3 +20,49 @@ const crossOffMovie = async (driver) => {
 };
 
 module.exports = {crossOffMovie};
+
+const {By} = require('selenium-webdriver')
+
+const deleteMovie = async (driver) => {
+    const addMovieInput = await driver.findElement(By.xpath('//input'));
+
+    addMovieInput.sendKeys('Back to the future');
+
+    const addMovieButton = await driver.findElement(By.xpath('//button'));
+
+    addMovieButton.click();
+    
+   await driver.sleep(1000);
+
+    const movie = await driver.findElement(By.xpath('//li'))
+
+    const isDisplayed = movie.isDisplayed();
+
+    expect(isDisplayed).toBeTruthy();
+    
+};
+
+module.exports = {deleteMovie};
+
+const {By} = require('selenium-webdriver')
+
+const addMovie = async (driver) => {
+    const addMovieInput = await driver.findElement(By.xpath('//input'));
+
+    addMovieInput.sendKeys('Back to the future');
+
+    const addMovieButton = await driver.findElement(By.xpath('//button'));
+
+    addMovieButton.click();
+    
+   await driver.sleep(1000);
+
+    const movie = await driver.findElement(By.xpath('//li'))
+
+    const isDisplayed = movie.isDisplayed();
+
+    expect(isDisplayed).toBeTruthy();
+    
+};
+
+module.exports = {addMovie};
